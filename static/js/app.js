0a5da107ab8c3ab14ca648c3ccf6879801e4f9e9
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-11-20 22:21:06 -0500

    trying to fetch data

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,12 @@
 // Read in data
-samplesUrl = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-samplesData = d3.json(samplesUrl);
 
-console.log(samplesData)
+// based on this answer: https://stackoverflow.com/a/14220323
+async function getData() {
+    const samplesUrl = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+    const samplesData = await d3.json(samplesUrl);
+    console.log(samplesData)
+    return samplesData
+};
+
+console.log(getData());
