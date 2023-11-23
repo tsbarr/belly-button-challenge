7f64bbd33c61d1fed4b30428ab94d621ff48e4db
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-11-23 17:58:09 -0500

    Finalize function docs

--- static/js/app.js
+++ static/js/app.js
@@ -4,21 +4,21 @@ const url =
 
 
 // Run initial function
-initOrUpdate('940', initial=true);
+initOrUpdate('940', initial = true);
 
 // Declare and define variable for dropdown
 const dropdown = d3.select("#selDataset");
-// Declare and define all spots where we should add the id (for plt titles)
+// Declare and define all spots where we should add the id (for plot titles)
 const labelId = d3.selectAll('.sampleID');
 
 // Listen for change in dropdown option
-dropdown.on("change", 
+dropdown.on("change",
   // when dropdrown changes
   () => {
     // Get value of selected id
     let newId = dropdown.property("value");
     // Update page
-    initOrUpdate(newId, initial=false);
+    initOrUpdate(newId, initial = false);
   }
 );
 
@@ -27,7 +27,6 @@ dropdown.on("change",
 * --- Functions --------------------------------------------------
 * ----------------------------------------------------------------
 */
-
 /**
  * Function that will display metadata and plots for selected id
  * @param {string} selectedId - The id of sample to display
@@ -38,8 +37,14 @@ function initOrUpdate(selectedId, initial) {
     // if promise is fulfilled:
     (data) => handleData(selectedId, data, initial),
     // if promise is rejected:
-    () => { console.log('data failed to load') });
+    () => {
+      console.log('data failed to load');
+      d3.selectAll('.plot-title')
+        .html('<h4>error: data failed to load</h4>');
+    }
+  );
 }
+// ---
 
 /**
  * Function for use within a .then() call, handles the data returned from a d3 promise
@@ -49,7 +54,7 @@ function initOrUpdate(selectedId, initial) {
  *                            If true (default): it sets the dropdown options and draws the plots
  *                            If false: it will only update the plots
  */
-function handleData(sampleId, dataSet, initial=true) {
+function handleData(sampleId, dataSet, initial = true) {
   labelId.text(sampleId);
   // Access data ids
   const ids = dataSet.names;
@@ -81,7 +86,7 @@ function handleData(sampleId, dataSet, initial=true) {
     }
   }
 }
-
+// ---
 /**
  * Function to add the ids to the dropdown menu, called by initOrUpdate when initial=true.
  * @param {string[]} idsObject - An array of strings that represent the sample ids,
@@ -97,8 +102,7 @@ function displayOptions(idsObject) {
   }
   dropdown.html(optionsHtml);
 }
-
-
+// ---
 /**
  * Function that takes a sample's metadata object and displays it in the html element with id=sample-metadata
  * @param {Object} sampleMetadata - An object containing a sample's metadata as key:value pairs
@@ -124,16 +128,23 @@ function displayMetadata(sampleMetadata) {
   // Display html in metadata panel
   metadataPanel.html(metadataHtml);
 }
-
-// ---
-function getTop10(sampleObject, reversed=true) {
+/**
+ * Function to calculate top 10 otus by value found in the selected sample
+ * @param {Object} sampleObject - object containing the selected sample's otu data
+ * @param {boolean} reversed - whether to reverse the top10 output 
+ *                            (default true for use in horizontal plotly bar charts)
+ * @returns Object containing the top 10 otus found in the sample by values, 
+ * as 3 named arrays: otu_ids, sample_values and otu_labels. 
+ * otu_ids are strings with 'OTU ' added before the otu id number
+ */
+function getTop10(sampleObject, reversed = true) {
   // Create an array that contains 3 arrays: 
   // 1 array for each: otu_ids, sample_values and otu_labels
-  const idValueLabel = Object.values(sampleObject).slice(1);
+  const sampleArray = Object.values(sampleObject).slice(1);
   // Transpose to create an array of arrays, one per otu
   // Based on: https://stackoverflow.com/a/17428705
-  const otus = idValueLabel[0].map(
-    (_, colIndex) => idValueLabel.map(row => row[colIndex])
+  const otus = sampleArray[0].map(
+    (_, colIndex) => sampleArray.map(row => row[colIndex])
   );
   // Sort desc otus based on sample_values (middle value of each otu array)
   otus.sort((a, b) => b[1] - a[1]);
@@ -143,22 +154,24 @@ function getTop10(sampleObject, reversed=true) {
   if (reversed) {
     top10.reverse();
   }
-
-  const top10Object = {
+  // Build object to return by separating each value type in 3 different arrays
+  const top10Return = {
     otu_ids: top10.map(x => `OTU ${x[0]}`),
     sample_values: top10.map(x => x[1]),
     otu_labels: top10.map(x => x[2])
   };
-
-  return top10Object;
+  return top10Return;
 }
-
 // ---
+/**
+ * Function to initialize the horizontal bar chart
+ * @param {Object} top10 - Object returned by getTop10(), contains the data to plot
+ */
 function initBar(top10) {
   let data = [{
-    y: top10.otu_ids, 
+    y: top10.otu_ids,
     x: top10.sample_values,
-    text: top10.otu_labels.map(x => x.replaceAll(';','<br>')),
+    text: top10.otu_labels.map(x => x.replaceAll(';', '<br>')),
     hoverinfo: 'text',
     type: 'bar',
     orientation: 'h',
@@ -170,20 +183,10 @@ function initBar(top10) {
   let config = { responsive: true };
   Plotly.newPlot('bar', data, layout, config);
 }
-
-// This function is called when a dropdown menu item is selected
-function updateBar(top10) {
-  // Set data update
-  let data_update = {
-    y: [top10.otu_ids],
-    x: [top10.sample_values],
-    text: [top10.otu_labels.map(x => x.replaceAll(';', '<br>'))]
-  };
-  // Update both data
-  Plotly.restyle('bar', data_update);
-}
-
-// ---
+/**
+ * Function to initialize the bubble plot
+ * @param {Object} sampleData - Object containing the selected sample's otu data
+ */
 function initBubble(sampleData) {
   let data = [{
     x: sampleData.otu_ids,
@@ -201,24 +204,10 @@ function initBubble(sampleData) {
   let config = { responsive: true };
   Plotly.newPlot('bubble', data, layout, config);
 }
-
-// This function is called when a dropdown menu item is selected
-function updateBubble(sampleData) {
-  // Set data update
-  let data_update = {
-    x: [sampleData.otu_ids],
-    y: [sampleData.sample_values],
-    text: [sampleData.otu_labels.map(x => x.replaceAll(';', '<br>'))],
-    marker: {
-      color: sampleData.otu_ids,
-      size: sampleData.sample_values
-    }
-  };
-  // Update both data and layout
-  Plotly.restyle('bubble', data_update);
-}
-
-// ---
+/**
+ * Function to initialize the gauge (i.e. indicator) plot
+ * @param {number} wfreq - value to use in gauge, from the sample's metadata
+ */
 function initGauge(wfreq) {
   let data = [
     {
@@ -227,7 +216,7 @@ function initGauge(wfreq) {
       type: "indicator",
       mode: "gauge+number",
       gauge: {
-        axis: { 
+        axis: {
           range: [0, 9],
           dtick: 1
         },
@@ -244,8 +233,46 @@ function initGauge(wfreq) {
   let config = { responsive: true };
   Plotly.newPlot('gauge', data, layout, config);
 }
-
-// This function is called when a dropdown menu item is selected
+// ---
+/**
+ * Function that updates the horizontal bar plot
+ * It is called when a new dropdown menu item is selected
+ * @param {Object} top10 - Object returned by getTop10(), contains the data to update the plot
+ */
+function updateBar(top10) {
+  // Set data update
+  let data_update = {
+    y: [top10.otu_ids],
+    x: [top10.sample_values],
+    text: [top10.otu_labels.map(x => x.replaceAll(';', '<br>'))]
+  };
+  // Update both data
+  Plotly.restyle('bar', data_update);
+}
+/**
+ * Function that updates the bubble plot
+ * It is called when a new dropdown menu item is selected
+ * @param {Object} sampleData - Object containing the selected sample's otu data
+ */
+function updateBubble(sampleData) {
+  // Set data update
+  let data_update = {
+    x: [sampleData.otu_ids],
+    y: [sampleData.sample_values],
+    text: [sampleData.otu_labels.map(x => x.replaceAll(';', '<br>'))],
+    marker: {
+      color: sampleData.otu_ids,
+      size: sampleData.sample_values
+    }
+  };
+  // Update both data and layout
+  Plotly.restyle('bubble', data_update);
+}
+/**
+ * Function that updates the gauge (i.e. indicator) plot
+ * It is called when a new dropdown menu item is selected
+ * @param {number} wfreq - value to use in gauge, from the sample's metadata
+ */
 function updateGauge(wfreq) {
   // Set data update
   let data_update = {
@@ -254,3 +281,4 @@ function updateGauge(wfreq) {
   // Update data trace
   Plotly.restyle('gauge', data_update);
 }
+// ---
